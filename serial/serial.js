5a63d891982b1fc58afea173d05b79b7c6bf300a
Cori Grohman <corigrohman@gmail.com>
2017-03-24 08:25:43 -0500

    initial commit

---
+++ serial/serial.js
@@ -0,0 +1,73 @@
+const _ = require('underscore');
+const serialport = require('serialport');
+var Readline = serialport.parsers.Readline;
+
+var portName = '/dev/ttyACM1';
+var sp       = new serialport(portName, {
+    baudRate: 115200
+});
+var parser   = sp.pipe(new Readline());
+
+function getDataString() {
+  var time = new Date().getTime();
+  var datestr = new Date(time - 0*36000000).toString().replace(/T/, ' ').replace(/Z/, '');
+  return datestr;
+}
+
+var currentTemp = {};
+var currentBrightness = {};
+parser.on('data', (input) => {
+  var reading = dataToObject(input);
+  if (!validateInputObj(reading)) return;
+  if (reading.type === 'temperature'){
+    currentTemp[reading.id] = {value: reading.value};
+  }
+  if (reading.type === 'brightness'){
+    currentBrightness[reading.id] = {value: reading.value};
+  }
+
+});
+
+var validateInputObj = (obj) => {
+  if (!obj.type || !obj.id){
+    console.log(`Data is not valid: ${obj}`);
+    return false;
+  }
+  if (obj.type === 'temperature'){
+    _.each(['value'], (key) => {
+      if (!obj[key]) return false;
+    }); 
+  }
+  if (obj.type === 'brightness'){
+    _.each(['value'], (key) =>{
+      if (!obj[key]) return false;
+    });
+  }
+  return true;
+};
+
+var dataToObject = (input) => {
+  var data = {};
+  _.each(input.split(','), (kv)=>{
+      var kv_list = kv.split(' ');
+      if (kv_list.length !== 2) return;
+      data[kv_list[0]] = kv_list[1].replace('\r','');
+    });
+  data.time = getDataString();
+  return data;
+};
+
+var getCurrentTemp = (id) => {
+  return currentTemp[id];
+}
+
+var getCurrentBrightness = (id) => {
+  return currentBrightness[id];
+}
+
+module.exports = {
+   currentTemp,
+   getCurrentTemp,
+   currentBrightness,
+   getCurrentBrightness
+};
---
+++ server/server.js
@@ -0,0 +1,110 @@
+var express    = require("express");
+var app        = express();
+var http       = require('http').Server(app);
+var bodyParser = require("body-parser");
+var gpio       = require("pi-gpio");
+var hbs        = require('express-hbs');
+var io         = require('socket.io')(http);
+var fs         = require('fs');
+var path       = require('path');
+
+var spawn      = require('child_process').spawn;
+var proc;
+
+var {currentTemp, getCurrentTemp, 
+     currentBrightness, getCurrentBrightness} = require('../serial/serial');
+
+app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname+'/views/partials' 
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname+'/views');
+  
+app.get('/', (req, res)=>{
+   var currentYear = new Date().getFullYear();
+   var obj =  {
+     pageTitle: "Home",
+     welcomeParagraph: "This demonstration shows an informative IoT sensor array.",
+     currentYear,
+   };
+   res.render("home.hbs", obj);
+});
+ 
+app.get('/about', (req, res)=>{
+   var currentYear = new Date().getFullYear();
+   res.render("about.hbs", {
+     pageTitle: "About Me",
+     welcomeParagraph: "Welcome!",
+     currentYear
+   });
+});
+ 
+app.post("/api/open", function(req, res){
+   var trigger = false;
+   gpio.open(26,"output", function(){
+      console.log("opened and now closing...");
+      gpio.write(26, 1);
+      });
+   res.send("Trying to toggle the LED...");
+});
+
+app.post("/api/close", function(req, res){
+   gpio.close(26);
+   res.send("Closing gpio");
+});
+
+var sockets = {};
+io.on('connection', (socket) => {
+  sockets[socket.id] = socket;
+  console.log(`Total connected users: ${Object.keys(sockets).length}`);
+
+  socket.on('disconnect', function() {
+    delete sockets[socket.id];
+    if (Object.keys(sockets).length === 0) {
+      app.set('watchingFile', false);
+      if (proc) proc.kill();
+      fs.unwatchFile('./public/image_stream.jpg');
+      console.log('There are 0 connections.');
+    }
+    console.log(`Total connected users: ${Object.keys(sockets).length}`);
+  });
+
+  socket.on('start-stream', function() {
+    startStreaming(io);
+  });
+
+});
+
+setInterval(()=>{
+  io.emit('temp value', getCurrentTemp(0));
+  io.emit('brightness', getCurrentBrightness(0))
+}, 1000);
+
+http.listen(3000, ()=>{
+   console.log("Server listening on 3000");
+});
+
+function startStreaming(io) {
+  if (app.get('watchingFile')){
+    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+    return;
+  }
+  var args = ["-w", "640", "-h", "480","-o", "./public/image_stream.jpg", "-t", "999999999", "-tl", "500"];
+  proc = spawn('raspistill', args);  
+
+  console.log('watching for changes...');
+  app.set('watchingFile', true);
+  fs.watchFile('./public/image_stream.jpg', function(current, previous){
+    io.sockets.emit('liveStream', 'image_stream.jpg?_t='+ (Math.random() * 100000));
+  }) 
+}
+
+
+
+
+
+
+
